852f9b75d1fbbde681b63de719370490be561326
Pavel Larin <www.larinpavel@mail.ru>
2023-12-01 02:40:58 +0500

    Initial commit with a clean and unadulterated run method...

---
+++ src/main/java/com/example/springdemodbpostgres/SpringDemoDbpostgresApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdemodbpostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoDbpostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoDbpostgresApplication.class, args);
+	}
+
+}
